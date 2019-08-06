2c856bc53cc6d27cc5a85c1366239f4524719f6b
martagrio <marta.igr90@gmail.com>
2019-08-06 19:16:18 +0200

    Add files from Thanos project

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+'use strict';
+const page = document.querySelector('.page');
+const title = document.querySelector('.title');
+const button = document.querySelector('.btn');
+
+const image = document.createElement('img');
+const chin = 'https://media3.giphy.com/media/Q8auEgoR7x0CcgH4uQ/giphy.gif?cid=790b76115d49a89c506a6c302e6e206c&rid=giphy.gif';
+const chinAlt = "you are saved, you can chill now";
+const snap = 'https://media2.giphy.com/media/3o6ozC2VM9R0XSMNKo/giphy.gif?cid=790b76115d49a837596474564d7ec7f3&rid=giphy.gif';
+const snapAlt = "you got killed, sorry";
+
+const message = document.createElement('h2');
+const spared = document.createTextNode('You were spared by Thanos.');
+const killed = document.createTextNode('You were slain by Thanos, for the good of the Universe.');
+
+
+function getRandom() {
+    return Math.floor(Math.random() * 2);
+}
+
+function destiny(fate, src, alt) {
+
+    message.appendChild(fate);
+    page.appendChild(message);
+    image.src = src;
+    image.alt = alt;
+    page.appendChild(image);
+}
+
+function killedOrNot() {
+    /* message.innerHTML = ''; */
+    button.classList.add('hidden');
+    title.classList.add('hidden');
+
+    getRandom();
+
+    if (getRandom() === 0) {
+        destiny(killed, snap, snapAlt);
+    } else {
+        destiny(spared, chin, chinAlt);
+    }
+
+}
+
+button.addEventListener('click', killedOrNot);
+
+localStorage.setItem('fate', messageInnerHTML);
